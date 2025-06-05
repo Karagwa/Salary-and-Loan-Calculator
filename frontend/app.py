e8bdba9d024a4f877a5af2dda943d6ab77de8c77
Ann Karagwa <annkaragwa@gmail.com>
2025-06-05 19:18:23 +0300

    fix: revert some changes to fix deployment

--- frontend/app.py
+++ frontend/app.py
@@ -3,8 +3,8 @@ import requests
 import pandas as pd
 from datetime import datetime
 
-
-BACKEND_URL = "https://salary-and-loan-calculator-lwqlvezyrnvkfmxhmdeedb.streamlit.app/"
+# Configure backend URL (will be overridden by Docker Compose)
+BACKEND_URL = "http://backend:8000"
 
 st.title("💰 Advanced Salary & Loan Calculator")
 
